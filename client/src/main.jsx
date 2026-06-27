ac2d594b90306cd30cee9a2315596bf6ecd12a32
zaidy-mughal <zaidamjadse@gmail.com>
2026-06-27 21:47:15 +0500

    feat(redux): updated the redux store to persist the state in the local storage using redux-persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 );
