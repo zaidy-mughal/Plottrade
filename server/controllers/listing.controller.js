09b5aaab58f7ea5bef2b08fa00cffa41198b174c
zaidy-mughal <zaidamjadse@gmail.com>
2026-07-08 12:36:33 +0500

    refactor(listing): updated the get user listing to handle user can access only own listings

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -113,11 +113,16 @@ export const getListings = async (req, res, next) => {
   }
 };
 
+
 export const getUserListings = async (req, res, next) => {
-  try {
-    const listings = await Listing.find({ userRef: req.user.id }).sort({ createdAt: "desc" });
-    return res.status(200).json(listings);
-  } catch (error) {
-    next(error);
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id }).sort({ createdAt: "desc" });
+      return res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
   }
 };
